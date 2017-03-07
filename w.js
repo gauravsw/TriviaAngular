fd083958247883ca99e65cdcd26756f8089e99c0
gauravsw <gaurav111shirodkar@gmail.com>
2017-03-07 17:24:57 +0530

    Add files via upload

---
+++ w.js
@@ -0,0 +1,45 @@
+var mdata = {
+	"qa" :
+		[
+			{
+				"question" : "The compare() method is declared in",
+				"options" : [	
+								"Comparable",
+								"Comparator",
+								"Compare",
+								"Compares"
+							],
+				"answer" : "Compares"		
+			},
+			{
+				"question" : "Which of the following methods returns List from an array?",
+				"options" : [
+								"Arrays.asList(Object[] array)",
+								"Arrays.list(Object[] array)",
+								"Arrays.toList(Object[] array)",
+								"Arrays.createList(Object[] array)"
+							],
+				"answer" : "Arrays.createList(Object[] array)"
+			},
+			{
+				"question" : "Which of the following classes is synchronized?",
+				"options" : [
+								"Vector",
+								"ArrayList",
+								"HashMap",
+								"LinkedList"
+							],
+				"answer" : "LinkedList"
+			},
+			{
+				"question" : "Which of the following methods returns an enumerator over the elements of a collection?",
+				"options" : [
+								"elements()",
+								"enumerator()",
+								"enumerate()",
+								"getElements()"
+							],
+				"answer" : "getElements()"
+			}
+		]	
+}
